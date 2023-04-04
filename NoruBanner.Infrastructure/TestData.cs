5b862246454b4ec9a5770806b5781a13f8eca1fb
Sabina <iamsabina21@gmail.com>
2023-04-04 23:36:20 +0500

    правки для запуска

--- NoruBanner.Infrastructure/TestData.cs
+++ NoruBanner.Infrastructure/TestData.cs
@@ -15,8 +15,11 @@ namespace NoruBanner.Infrastructure
             }
             if (!await context.Banners.AnyAsync())
             {
-                var banner = new Banner(Guid.Parse("0fa85f64-5717-4562-b3fc-2c963f66afa6") , "https://sark.ws", "banner.PNG", "image/png");
-                await context.Banners.AddAsync(banner);
+                var banners = new List<Banner>() {
+                    new Banner(Guid.Parse("0fa85f64-5717-4562-b3fc-2c963f66afa6") , "https://sark.ws", "banner2.PNG", "image/png"),
+                    new Banner(Guid.Parse("ff84e3a2-3e0b-460d-b1cc-d414400b4f5d") , "https://tiburon-research.ru/consumer-behavior-monitoring", "banner.PNG", "image/png")
+                };
+                await context.Banners.AddRangeAsync(banners);
             }
             await context.SaveChangesAsync();
         }
